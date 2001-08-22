6f0038a8629aea0e8f2cf5913c033ea5af6ce430
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2001-08-22 16:29:20 +0000

    nly modifies changed files, temp file in tmp directory used, can copy files, if renaming doesn't work.

--- LineEnds.java
+++ LineEnds.java
@@ -25,7 +25,7 @@ import gnu.getopt.*;
  * that of a given platform.
  */
 public class LineEnds {
-    public static String version = "1.0";
+    public static String version = "1.1";
 
     /**
      * Converts the line ending on files, or standard input.
@@ -45,19 +45,24 @@ public class LineEnds {
             new LongOpt("java", LongOpt.NO_ARGUMENT, null, 'n'),
             new LongOpt("mac", LongOpt.NO_ARGUMENT, null, 'r'),             
             new LongOpt("system", LongOpt.NO_ARGUMENT, null, 's'),            
-            new LongOpt("force", LongOpt.NO_ARGUMENT, null, 'f'),            
+            new LongOpt("force", LongOpt.NO_ARGUMENT, null, 'f'),              
+            new LongOpt("quiet", LongOpt.NO_ARGUMENT, null, 'q'),             
+            new LongOpt("reallyquiet", LongOpt.NO_ARGUMENT, null, 'Q'),             
+            new LongOpt("verbose", LongOpt.NO_ARGUMENT, null, 'v'),            
             new LongOpt("noforce", LongOpt.NO_ARGUMENT, null, 4),
         };
-        Getopt opts = new Getopt("LineEnds", args, "d:n:r:s:f:", longopts);
+        Getopt opts = new Getopt("LineEnds", args, "dnrsfqQv", longopts);
         int style = STYLE_SYSTEM; 
         boolean force = false;
+        boolean printMessages = false;
+        boolean printErrors = true;
         int c;
         while ((c = opts.getopt()) != -1){
             switch(c){
           		case 1:{
                     // print out the help message
                 	System.out.println(
-                        "LineEnds [-dnrsf] <files>\n\n" +
+                        "LineEnds [-dnrsfvqQ] <files>\n\n" +
                         "   Adjusts the line endings in files.\n" +
                         "   If no files are specified standard in and out will be used.\n\n" +
                         "   --help                 Print this help message.\n" +
@@ -68,7 +73,10 @@ public class LineEnds {
                         "   -r --mac               Use the Macintosh line ending.\n" +
                         "   -s --system            Use the current system's line ending. (default)\n" +
                         "   -f --force             Always modify files, even binary files.\n" +
-                        "   --noforce              Don't modify binary files. (default)\n"
+                        "   --noforce              Don't modify binary files. (default)\n" +
+                        "   -v --verbose           Print a message for each file modified.\n" +
+                        "   -q --quiet             Print error messages. (default)\n" +
+                        "   -Q --reallyquiet       Print nothing.\n"
 					);
                     System.exit(0);
                 } break;
@@ -117,6 +125,21 @@ public class LineEnds {
                 case 4:{
                     force = false;
                 } break;
+                case 'v':{
+                    printMessages = true;
+                    printErrors = true;
+                } break;
+                case 'q':{
+                    printMessages = false;
+                    printErrors = true;
+                } break;
+                case 'Q':{
+                    printMessages = false;
+                    printErrors = false;
+                } break;
+                default:{
+                    System.exit(1);
+                }
             }
         } 
         
@@ -128,21 +151,39 @@ public class LineEnds {
             File source = null;
             InputStream in = null;
             OutputStream out = null;
+            boolean modified = false;
             try {
                 source = new File(args[i]); 
                 source = source.getCanonicalFile();
                 in = new FileInputStream(source);  
                 temp = File.createTempFile("LineEnds", null, null);
                 out = new FileOutputStream(temp);
-                convert (in, out, style, !force);
+                modified = convert (in, out, style, !force);
                 in.close();
                 in = null;
                 out.flush();
                 out.close();
                 out = null;
-                source.delete();    
-                temp.renameTo(source);
-                temp = null;                             
+                if (modified){
+                    if (!source.delete() || !temp.renameTo(source)){                        
+                        String message = "Could not be modified";
+                        try {
+                            in = new FileInputStream(temp);  
+                            out = new FileOutputStream(source);
+                            message += " and is likely corrupted";
+                            copy(in, out);
+                            temp.deleteOnExit();
+                        } catch (IOException x1){
+                            throw new IOException(message + ". Modifications can be found in " + temp);
+                        }                        
+                    } // else temp renamed so don't delete 
+                    if (printMessages){
+                        System.out.println("Modified line breaks in: " + args[i]);
+                    }  
+                } else {
+                    temp.deleteOnExit(); 
+                }
+                temp = null;                 
             } catch (Exception x){                
                 if (in != null){
                     try { 
@@ -157,10 +198,12 @@ public class LineEnds {
                     } catch (IOException ignore){
                     }
                 }  
-                if (temp != null && source.exists() && temp.exists()){
-                    temp.delete();                    
-                }                               
-                System.err.println(args[i] + ": " + x.getMessage());
+                if (!modified && temp != null && temp.exists()){
+                    temp.deleteOnExit();                    
+                } 
+                if(printErrors){                              
+                    System.err.println(args[i] + ": " + x.getMessage());
+                }
                 exitCond = 1; 
             }                        
         }
@@ -217,10 +260,13 @@ public class LineEnds {
      * Buffer size when reading from input stream.
      */
     private final static int BUFFER_SIZE = 1024;
-    
     private final static int STATE_INIT = 0;
-    private final static int STATE_R = 1;
+    private final static int STATE_R = 1; 
     
+    private final static int MASK_N = 0x01;
+    private final static int MASK_R = 0x02;
+    private final static int MASK_RN = 0x04;
+   
     /**
      * Change the line endings of the text on the input stream and write 
      * it to the output stream.
@@ -229,11 +275,12 @@ public class LineEnds {
      *
      * @param in stream that contains the text which needs line number conversion.
      * @param out stream where converted text is written.
+     * @return true if the output was modified from the input, false if it is exactly the same
      * @throws BinaryDataException if non-text data is encountered.
      * @throws IOException if an input or output error occurs.
      */
-    public static void convert(InputStream in, OutputStream out) throws IOException {
-        convert(in, out, STYLE_SYSTEM, true);
+    public static boolean convert(InputStream in, OutputStream out) throws IOException {
+        return convert(in, out, STYLE_SYSTEM, true);
     }
     
     /**
@@ -243,13 +290,13 @@ public class LineEnds {
      * @param in stream that contains the text which needs line number conversion.
      * @param out stream where converted text is written.
      * @param style line separator style.
-     * @param binaryException throw an exception and abort the operation if binary data is encountered.
+     * @return true if the output was modified from the input, false if it is exactly the same
      * @throws BinaryDataException if non-text data is encountered.
      * @throws IOException if an input or output error occurs.
      * @throws IllegalArgumentException if an unknown style is requested.
      */
-    public static void convert(InputStream in, OutputStream out, int style) throws IOException {
-        convert(in, out, style, true);
+    public static boolean convert(InputStream in, OutputStream out, int style) throws IOException {
+        return convert(in, out, style, true);
     }
     
     /**
@@ -260,13 +307,13 @@ public class LineEnds {
      *
      * @param in stream that contains the text which needs line number conversion.
      * @param out stream where converted text is written.
-     * @param style line separator style.
      * @param binaryException throw an exception and abort the operation if binary data is encountered and binaryExcepion is false.
+     * @return true if the output was modified from the input, false if it is exactly the same
      * @throws BinaryDataException if non-text data is encountered.
      * @throws IOException if an input or output error occurs.
      */
-    public static void convert(InputStream in, OutputStream out, boolean binaryException) throws IOException {
-        convert(in, out, STYLE_SYSTEM, binaryException);
+    public static boolean convert(InputStream in, OutputStream out, boolean binaryException) throws IOException {
+        return convert(in, out, STYLE_SYSTEM, binaryException);
     }
     
     /**
@@ -277,11 +324,12 @@ public class LineEnds {
      * @param out stream where converted text is written.
      * @param style line separator style.
      * @param binaryException throw an exception and abort the operation if binary data is encountered and binaryExcepion is false.
+     * @return true if the output was modified from the input, false if it is exactly the same
      * @throws BinaryDataException if non-text data is encountered.
      * @throws IOException if an input or output error occurs.
      * @throws IllegalArgumentException if an unknown style is requested.
      */
-    public static void convert(InputStream in, OutputStream out, int style, boolean binaryException) throws IOException {
+    public static boolean convert(InputStream in, OutputStream out, int style, boolean binaryException) throws IOException {
         byte[] lineEnding;
         switch (style) {
             case STYLE_SYSTEM: {
@@ -303,30 +351,62 @@ public class LineEnds {
         byte[] buffer = new byte[BUFFER_SIZE];
         int read;
         int state = STATE_INIT;
+        int seen = 0x00;
         while((read = in.read(buffer)) != -1){
             for (int i=0; i<read; i++){
                 byte b = buffer[i];
                 if (state==STATE_R){
                     if(b!='\n'){
                         out.write(lineEnding);
+                        seen |= MASK_R;
                     }
                 }
                 if (b=='\r'){
                     state = STATE_R;
                 } else {
-                    state = STATE_INIT;
                     if (b=='\n'){
+                        if (state==STATE_R){                            
+                            seen |= MASK_RN;
+                        } else {                            
+                            seen |= MASK_N;
+                        }
                         out.write(lineEnding);
                     } else if(binaryException && b!='\t' && b!='\f' && (b & 0xff)<32){
                         throw new BinaryDataException("Binary data encountered, line break replacement aborted.");
                     } else {
                         out.write(b);
-                    }
+                    }                    
+                    state = STATE_INIT;
                 }
             }
         }
         if (state==STATE_R){
             out.write(lineEnding);
+            seen |= MASK_R;
+        }
+        if (lineEnding.length==2 && lineEnding[0]=='\r' && lineEnding[1]=='\n'){
+            return ((seen & ~MASK_RN)!=0);
+        } else if (lineEnding.length==1 && lineEnding[0]=='\r'){
+            return ((seen & ~MASK_R)!=0);
+        } else if (lineEnding.length==1 && lineEnding[0]=='\n'){
+            return ((seen & ~MASK_N)!=0);
+        } else {
+            return true;
+        }
+    }
+    
+    /**
+     * Copy the date from the input stream to the output stream.
+     *
+     * @param in data source
+     * @param out data destination
+     * @throws IOException in an input or output error occurs
+     */
+    private static void copy(InputStream in, OutputStream out) throws IOException{
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
         }
     }
 }
