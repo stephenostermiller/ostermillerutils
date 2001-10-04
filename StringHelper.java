16f2cc26eb84ac714dfaeb74b6fb1ff876bac2fa
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2001-10-04 00:05:53 +0000

    Added split method, improved documentation, removed main method.

--- StringHelper.java
+++ StringHelper.java
@@ -18,66 +18,144 @@
 package com.Ostermiller.util;
 
 /** 
- * Utilities for String formatting and queries.
+ * Utilities for String formatting, manipulation, and queries.
  */
 public class StringHelper {
 
-    /**
-     *
-     */
-    public static void main(String[] args){
-        for (int i=0; i<args.length; i++){
-            System.out.println(prepad(args[i], 10, '*'));
-            System.out.println(postpad(args[i], 10, '*'));            
-        }
-    }
-    
-    /**
-     * Prepend the given character to string until
-     * the result is long enough.
-     *
-     * If a string is too long, it will not be trunkated,
-     * however no padding will be added.
-     *
-     * @param s String to be padded.
-     * @param length desired length of result.
-     * @param c padding character
-     * @return padded string.
-     */
-    public static String prepad(String s, int length, char c){
-        int needed = length - s.length();
-        if (needed <= 0){
-            return s;
-        }
-        StringBuffer sb = new StringBuffer(length);
-        for (int i=0; i<needed; i++){
-            sb.append(c);
-        }
-        sb.append(s);
-        return (sb.toString());
-    }
-    /**
-     * Append the given character to string until
-     * the result is long enough.
-     *
-     * If a string is too long, it will not be trunkated,
-     * however no padding will be added.
-     *
-     * @param s String to be padded.
-     * @param length desired length of result.
-     * @param c padding character
-     * @return padded string.
-     */
-    public static String postpad(String s, int length, char c){
-        int needed = length - s.length();
-        if (needed <= 0){
-            return s;
-        }
-        StringBuffer sb = new StringBuffer(length);        
-        sb.append(s);
-        for (int i=0; i<needed; i++){
-            sb.append(c);
-        }
-        return (sb.toString());
-    }
+	/**
+	 * Prepend the given character to the String until
+	 * the result is the desired length.
+	 * <p>
+	 * If a String is longer than the desired length,
+	 * it will not be trunkated, however no padding
+	 * will be added.
+	 *
+	 * @param s String to be padded.
+	 * @param length desired length of result.
+	 * @param c padding character.
+	 * @return padded String.
+	 * @throws NullPointerException if s is null.
+	 */
+	public static String prepad(String s, int length, char c){
+		int needed = length - s.length();
+		if (needed <= 0){
+			return s;
+		}
+		StringBuffer sb = new StringBuffer(length);
+		for (int i=0; i<needed; i++){
+			sb.append(c);
+		}
+		sb.append(s);
+		return (sb.toString());
+	}
+	/**
+	 * Append the given character to the String until
+	 * the result is  the desired length.
+	 * <p>
+	 * If a String is longer than the desired length,
+	 * it will not be trunkated, however no padding
+	 * will be added.
+	 *
+	 * @param s String to be padded.
+	 * @param length desired length of result.
+	 * @param c padding character.
+	 * @return padded String.
+	 * @throws NullPointerException if s is null.
+	 */
+	public static String postpad(String s, int length, char c){
+		int needed = length - s.length();
+		if (needed <= 0){
+			return s;
+		}
+		StringBuffer sb = new StringBuffer(length);
+		sb.append(s);
+		for (int i=0; i<needed; i++){
+			sb.append(c);
+		}
+		return (sb.toString());
+	}
+
+	/**
+	 * Split the given String into tokens.
+	 * <P>
+	 * This method is meant to be similar to the split
+	 * function in other programming languages but it does
+	 * not use regular expressions.  Rather the String is
+	 * split on a single String literal.
+	 * <P>
+	 * Unlike java.util.StringTokenizer which accepts
+	 * multiple character tokens as delimiters, the delimiter
+	 * here is a single String literal.
+	 * <P>
+	 * Each null token is returned as an empty String.
+	 * Delimiters are never returned as tokens.
+	 * <P>
+	 * If there is no delimiter because it is either empty or
+	 * null, the only element in the result is the original String.
+	 * <P>
+	 * StringHelper.split("1-2-3", "-");<br>
+	 * result: {"1", "2", "3"}<br>
+	 * StringHelper.split("-1--2-", "-");<br>
+	 * result: {"", "1", ,"", "2", ""}<br>
+	 * StringHelper.split("123", "");<br>
+	 * result: {"123"}<br>
+	 * StringHelper.split("1-2---3----4", "--");<br>
+	 * result: {"1-2", "-3", "", "4"}<br>
+	 *
+	 * @param s String to be split.
+	 * @param delimiter String literal on which to split.
+	 * @return an array of tokens.
+	 * @throws NullPointerException if s is null.
+	 */
+	public static String[] split(String s, String delimiter){
+		int delimiterLength;
+		// the next statement has the side effect of throwing a null pointer
+		// exception if s is null.
+		int stringLength = s.length();
+		if (delimiter == null || (delimiterLength = delimiter.length()) == 0){
+			// it is not inherently clear what to do if there is no delimiter
+			// On one hand it would make sense to return each character because
+			// the null String can be found between each pair of characters in
+			// a String.  However, it can be found many times there and we don't
+			// want to be returning multiple null tokens.
+			// returning the whole String will be defined as the correct behavior
+			// in this instance.
+			return new String[] {s};
+		}
+
+		// a two pass solution is used because a one pass solution would
+		// require the possible resizing and copying of memory structures
+		// In the worst case it would have to be resized n times with each
+		// resize having a O(n) copy leading to an O(n^2) algorithm.
+
+		int count;
+		int start;
+		int end;
+
+		// Scan s and count the tokens.
+		count = 0;
+		start = 0;
+		while((end = s.indexOf(delimiter, start)) != -1) {
+			count++;
+			start = end + delimiterLength;
+		}
+		count++;
+
+		// allocate an array to return the tokens,
+		// we now know how big it should be
+		String[] result = new String[count];
+
+		// Scan s again, but this time pick out the tokens
+		count = 0;
+		start = 0;
+		while((end = s.indexOf(delimiter, start)) != -1) {
+			result[count] = (s.substring(start, end));
+			count++;
+			start = end + delimiterLength;
+		}
+		end = stringLength;
+		result[count] = s.substring(start, end);
+
+		return (result);
+	}
 }
