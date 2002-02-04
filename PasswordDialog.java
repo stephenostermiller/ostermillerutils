2c99061e1d1821e150bd3f89ab46d569c2c49a0e
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2002-02-04 13:51:51 +0000

    Escape key is the same as cancel, enter keys work on buttons.

--- PasswordDialog.java
+++ PasswordDialog.java
@@ -1,6 +1,6 @@
 /*
  * A password dialog box.
- * Copyright (C) 2001 Stephen Ostermiller <utils@Ostermiller.com>
+ * Copyright (C) 2001, 2002 Stephen Ostermiller <utils@Ostermiller.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,304 +32,325 @@ import java.util.Locale;
  * <pre>
  * PasswordDialog p = new PasswordDialog(null, "Test");
  * if(p.showDialog()){
- *	   System.out.println("Name: " + p.getName());
- *	   System.out.println("Pass: " + p.getPass());
+ *     System.out.println("Name: " + p.getName());
+ *     System.out.println("Pass: " + p.getPass());
  * } else {
- *	   System.out.println("User selected cancel");
+ *     System.out.println("User selected cancel");
  * }
  * </pre>
  * </code>
  */
 public class PasswordDialog extends JDialog {
 
-	/**
-	 * Locale specific strings displayed to the user.
-	 */
-	protected ResourceBundle labels;
+    /**
+     * Locale specific strings displayed to the user.
+     */
+    protected ResourceBundle labels;
 
-	/**
-	 * Set the locale used for getting localized 
-	 * strings.
-	 *
-	 * @param locale Locale used to for i18n.
-	 */
-	public void setLocale(Locale locale){
-		labels = ResourceBundle.getBundle("com.Ostermiller.util.PasswordDialog",  locale);
-	}
-	
-	/**
-	 * Where the name is typed.
-	 */
-	protected JTextField name;
-	/**
-	 * Where the password is typed.
-	 */
-	protected JPasswordField pass;
-	/**
-	 * The OK button.
-	 */
-	protected JButton okButton;
-	/**
-	 * The cancel button.
-	 */
-	protected JButton cancelButton;
-	/**
-	 * The label for the field in which the name is typed.
-	 */
-	protected JLabel nameLabel;
-	/**
-	 * The label for the field in which the password is typed.
-	 */
-	protected JLabel passLabel;
+    /**
+     * Set the locale used for getting localized 
+     * strings.
+     *
+     * @param locale Locale used to for i18n.
+     */
+    public void setLocale(Locale locale){
+        labels = ResourceBundle.getBundle("com.Ostermiller.util.PasswordDialog",  locale);
+    }
+    
+    /**
+     * Where the name is typed.
+     */
+    protected JTextField name;
+    /**
+     * Where the password is typed.
+     */
+    protected JPasswordField pass;
+    /**
+     * The OK button.
+     */
+    protected JButton okButton;
+    /**
+     * The cancel button.
+     */
+    protected JButton cancelButton;
+    /**
+     * The label for the field in which the name is typed.
+     */
+    protected JLabel nameLabel;
+    /**
+     * The label for the field in which the password is typed.
+     */
+    protected JLabel passLabel;
 
-	/**
-	 * Set the name that appears as the default
-	 * An empty string will be used if this in not specified
-	 * before the dialog is displayed.
-	 *
-	 * @param name default name to be displayed.
-	 */
-	public void setName(String name){
-		this.name.setText(name);
-	}
+    /**
+     * Set the name that appears as the default
+     * An empty string will be used if this in not specified
+     * before the dialog is displayed.
+     *
+     * @param name default name to be displayed.
+     */
+    public void setName(String name){
+        this.name.setText(name);
+    }
 
-	/**
-	 * Set the password that appears as the default
-	 * An empty string will be used if this in not specified
-	 * before the dialog is displayed.
-	 *
-	 * @param pass default password to be displayed.
-	 */
-	public void setPass(String pass){
-		this.pass.setText(pass);
-	}
+    /**
+     * Set the password that appears as the default
+     * An empty string will be used if this in not specified
+     * before the dialog is displayed.
+     *
+     * @param pass default password to be displayed.
+     */
+    public void setPass(String pass){
+        this.pass.setText(pass);
+    }
 
-	/**
-	 * Set the label on the ok button.
-	 * The default is a localized string.
-	 *
-	 * @param ok label for the ok button.
-	 */
-	public void setOKText(String ok){
-		this.okButton.setText(ok);
-		pack();
-	}
+    /**
+     * Set the label on the ok button.
+     * The default is a localized string.
+     *
+     * @param ok label for the ok button.
+     */
+    public void setOKText(String ok){
+        this.okButton.setText(ok);
+        pack();
+    }
 
-	/**
-	 * Set the label on the cancel button.
-	 * The default is a localized string.
-	 *
-	 * @param cancel label for the cancel button.
-	 */
-	public void setCancelText(String cancel){
-		this.cancelButton.setText(cancel);
-		pack();
-	}
+    /**
+     * Set the label on the cancel button.
+     * The default is a localized string.
+     *
+     * @param cancel label for the cancel button.
+     */
+    public void setCancelText(String cancel){
+        this.cancelButton.setText(cancel);
+        pack();
+    }
 
-	/**
-	 * Set the label for the field in which the name is entered.
-	 * The default is a localized string.
-	 *
-	 * @param name label for the name field.
-	 */
-	public void setNameLabel(String name){
-		this.nameLabel.setText(name);
-		pack();
-	}
+    /**
+     * Set the label for the field in which the name is entered.
+     * The default is a localized string.
+     *
+     * @param name label for the name field.
+     */
+    public void setNameLabel(String name){
+        this.nameLabel.setText(name);
+        pack();
+    }
 
-	/**
-	 * Set the label for the field in which the password is entered.
-	 * The default is a localized string.
-	 *
-	 * @param pass label for the password field.
-	 */
-	public void setPassLabel(String pass){
-		this.passLabel.setText(pass);
-		pack();
-	}
+    /**
+     * Set the label for the field in which the password is entered.
+     * The default is a localized string.
+     *
+     * @param pass label for the password field.
+     */
+    public void setPassLabel(String pass){
+        this.passLabel.setText(pass);
+        pack();
+    }
 
-	/**
-	 * Get the name that was entered into the dialog before
-	 * the dialog was closed.
-	 *
-	 * @return the name from the name field.
-	 */
-	public String getName(){
-		return name.getText();
-	}
+    /**
+     * Get the name that was entered into the dialog before
+     * the dialog was closed.
+     *
+     * @return the name from the name field.
+     */
+    public String getName(){
+        return name.getText();
+    }
 
-	/**
-	 * Get the password that was entered into the dialog before
-	 * the dialog was closed.
-	 *
-	 * @return the password from the password field.
-	 */
-	public String getPass(){
-		return new String(pass.getPassword());
-	}
+    /**
+     * Get the password that was entered into the dialog before
+     * the dialog was closed.
+     *
+     * @return the password from the password field.
+     */
+    public String getPass(){
+        return new String(pass.getPassword());
+    }
 
-	/**
-	 * Finds out if user used the OK button or an equivelant action
-	 * to close the dialog.
-	 * Pressing enter in the password field may be the same as
-	 * 'OK' but closing the dialog and pressing the cancel button
-	 * are not.
-	 *
-	 * @return true if the the user hit OK, false if the user cancelled.
-	 */
-	public boolean okPressed(){
-		return pressed_OK;
-	}
+    /**
+     * Finds out if user used the OK button or an equivelant action
+     * to close the dialog.
+     * Pressing enter in the password field may be the same as
+     * 'OK' but closing the dialog and pressing the cancel button
+     * are not.
+     *
+     * @return true if the the user hit OK, false if the user cancelled.
+     */
+    public boolean okPressed(){
+        return pressed_OK;
+    }
 
-	/**
-	 * update this variable when the user makes an action
-	 */
-	private boolean pressed_OK = false;
+    /**
+     * update this variable when the user makes an action
+     */
+    private boolean pressed_OK = false;
 
-	/**
-	 * Create this dialog with the given parent and title.
-	 *
-	 * @param parent window from which this dialog is launched
-	 * @param title the title for the dialog box window
-	 */
-	public PasswordDialog(Frame parent, String title) {
+    /**
+     * Create this dialog with the given parent and title.
+     *
+     * @param parent window from which this dialog is launched
+     * @param title the title for the dialog box window
+     */
+    public PasswordDialog(Frame parent, String title) {
 
-		super(parent, title, true);
+        super(parent, title, true);
 
-		if (labels == null){
-			setLocale(Locale.getDefault());
-		}
+        if (labels == null){
+            setLocale(Locale.getDefault());
+        }
 
-		if (title==null){
-			setTitle(labels.getString("dialog.title"));
-		}
-		if (parent != null){
-			setLocationRelativeTo(parent);
-		}
-		// super calls dialogInit, so we don't need to do it again.
-	}
+        if (title==null){
+            setTitle(labels.getString("dialog.title"));
+        }
+        if (parent != null){
+            setLocationRelativeTo(parent);
+        }
+        // super calls dialogInit, so we don't need to do it again.
+    }
 
-	/**
-	 * Create this dialog with the given parent and the default title.
-	 *
-	 * @param parent window from which this dialog is launched
-	 */
-	public PasswordDialog(Frame parent) {
-		this(parent, null);
-	}
+    /**
+     * Create this dialog with the given parent and the default title.
+     *
+     * @param parent window from which this dialog is launched
+     */
+    public PasswordDialog(Frame parent) {
+        this(parent, null);
+    }
 
-	/**
-	 * Create this dialog with the default title.
-	 */
-	public PasswordDialog() {
-		this(null, null);
-	}
+    /**
+     * Create this dialog with the default title.
+     */
+    public PasswordDialog() {
+        this(null, null);
+    }
 
-	/**
-	 * Called by constructors to initialize the dialog.
-	 */
-	protected void dialogInit(){
+    /**
+     * Called by constructors to initialize the dialog.
+     */
+    protected void dialogInit(){
 
-		if (labels == null){
-			setLocale(Locale.getDefault());
-		}
+        if (labels == null){
+            setLocale(Locale.getDefault());
+        }
 
-		name = new JTextField("", 20);
-		pass = new JPasswordField("", 20);
-		okButton = new JButton(labels.getString("dialog.ok"));
-		cancelButton = new JButton(labels.getString("dialog.cancel"));
-		nameLabel = new JLabel(labels.getString("dialog.name"));
-		passLabel = new JLabel(labels.getString("dialog.pass"));
+        name = new JTextField("", 20);
+        pass = new JPasswordField("", 20);
+        okButton = new JButton(labels.getString("dialog.ok"));
+        cancelButton = new JButton(labels.getString("dialog.cancel"));
+        nameLabel = new JLabel(labels.getString("dialog.name") + " ");
+        passLabel = new JLabel(labels.getString("dialog.pass") + " ");
 
-		super.dialogInit();
+        super.dialogInit();
 
-		ActionListener actionListener = new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				Object source = e.getSource();
-				if (source == name){
-					// the user pressed enter in the name field.
-					name.transferFocus();
-				} else {
-					// other actions close the dialog.
-					pressed_OK = (source == pass || source == okButton);
-					PasswordDialog.this.hide();
-				}
-			}
-		};
+        KeyListener keyListener = (new KeyAdapter() {
+            public void keyPressed(KeyEvent e){
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE ||
+                        (e.getSource() == cancelButton
+                        && e.getKeyCode() == KeyEvent.VK_ENTER)){
+                    pressed_OK = false;
+                    PasswordDialog.this.hide();
+                }
+                if (e.getSource() == okButton &&
+                        e.getKeyCode() == KeyEvent.VK_ENTER){
+                    pressed_OK = true;
+                    PasswordDialog.this.hide();
+                }
+            }
+        });
+        addKeyListener(keyListener);
 
-		GridBagLayout gridbag = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();
-		c.insets.top = 5;
-		c.insets.bottom = 5;
-		JPanel pane = new JPanel(gridbag);
-		pane.setBorder(BorderFactory.createEmptyBorder(10, 20, 5, 20));
-		JLabel label;
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                Object source = e.getSource();
+                if (source == name){
+                    // the user pressed enter in the name field.
+                    name.transferFocus();
+                } else {
+                    // other actions close the dialog.
+                    pressed_OK = (source == pass || source == okButton);
+                    PasswordDialog.this.hide();
+                }
+            }
+        };
 
-		c.anchor = GridBagConstraints.EAST;
-		gridbag.setConstraints(nameLabel, c);
-		pane.add(nameLabel);
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets.top = 5;
+        c.insets.bottom = 5;
+        JPanel pane = new JPanel(gridbag);
+        pane.setBorder(BorderFactory.createEmptyBorder(10, 20, 5, 20));
+        JLabel label;
 
-		gridbag.setConstraints(name, c);
-		name.addActionListener(actionListener);
-		pane.add(name);
+        c.anchor = GridBagConstraints.EAST;
+        gridbag.setConstraints(nameLabel, c);
+        pane.add(nameLabel);
 
-		c.gridy = 1;
-		gridbag.setConstraints(passLabel, c);
-		pane.add(passLabel);
+        gridbag.setConstraints(name, c);
+        name.addActionListener(actionListener);
+        name.addKeyListener(keyListener);
+        pane.add(name);
 
-		gridbag.setConstraints(pass, c);
-		pass.addActionListener(actionListener);
-		pane.add(pass);
+        c.gridy = 1;
+        gridbag.setConstraints(passLabel, c);
+        pane.add(passLabel);
 
-		c.gridy = 2;
-		c.gridwidth = GridBagConstraints.REMAINDER;
-		c.anchor = GridBagConstraints.CENTER;
-		JPanel panel = new JPanel();
-		okButton.addActionListener(actionListener);
-		panel.add(okButton);
-		cancelButton.addActionListener(actionListener);
-		panel.add(cancelButton);
-		gridbag.setConstraints(panel, c);
-		pane.add(panel);
+        gridbag.setConstraints(pass, c);
+        pass.addActionListener(actionListener);
+        pass.addKeyListener(keyListener);
+        pane.add(pass);
 
-		getContentPane().add(pane);
+        c.gridy = 2;
+        c.gridwidth = GridBagConstraints.REMAINDER;
+        c.anchor = GridBagConstraints.CENTER;
+        JPanel panel = new JPanel();
+        okButton.addActionListener(actionListener);
+        okButton.addKeyListener(keyListener);
+        panel.add(okButton);
+        cancelButton.addActionListener(actionListener);
+        cancelButton.addKeyListener(keyListener);
+        panel.add(cancelButton);
+        gridbag.setConstraints(panel, c);
+        pane.add(panel);
 
-		pack();
-	}
+        getContentPane().add(pane);
 
-	/**
-	 * Shows the dialog and returns true if the user pressed ok.
-	 *
-	 * @return true if the the user hit OK, false if the user cancelled.
-	 */
-	public boolean showDialog(){
-		show();
-		return okPressed();
-	}
+        pack();
+    }
 
-	/**
-	 * A simple example to show how this might be used.
-	 * If there are arguments passed to this program, the first
-	 * is treated as the default name, the second as the default password
-	 *
-	 * @param args command line arguments: name and password (optional)
-	 */
-	private static void main(String[] args){
-		PasswordDialog p = new PasswordDialog();
-		if(args.length > 0){
-			p.setName(args[0]);
-		}
-		if(args.length > 1){
-			p.setPass(args[1]);
-		}
-		if(p.showDialog()){
-			System.out.println("Name: " + p.getName());
-			System.out.println("Pass: " + p.getPass());
-		} else {
-			System.out.println("User selected cancel");
-		}
-		p.dispose();
-		p = null;
-		System.exit(0);
-	}
+    /**
+     * Shows the dialog and returns true if the user pressed ok.
+     *
+     * @return true if the the user hit OK, false if the user cancelled.
+     */
+    public boolean showDialog(){
+        show();
+        return okPressed();
+    }
+
+    /**
+     * A simple example to show how this might be used.
+     * If there are arguments passed to this program, the first
+     * is treated as the default name, the second as the default password
+     *
+     * @param args command line arguments: name and password (optional)
+     */
+    private static void main(String[] args){
+        PasswordDialog p = new PasswordDialog();
+        if(args.length > 0){
+            p.setName(args[0]);
+        }
+        if(args.length > 1){
+            p.setPass(args[1]);
+        }
+        if(p.showDialog()){
+            System.out.println("Name: " + p.getName());
+            System.out.println("Pass: " + p.getPass());
+        } else {
+            System.out.println("User selected cancel");
+        }
+        p.dispose();
+        p = null;
+        System.exit(0);
+    }
 }
