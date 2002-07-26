c275bef1bc4f9b9146a0ac67c6ee26693482ad8b
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2002-07-26 06:54:16 +0000

    Release script with sanity checking.

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+size=`ls -lah utils.jar`
+size=${size:38:4}
+echo $size
+if [ -z "`grep $size index.html`" ]
+then
+    echo "utils.jar size is $size but index.html does not show that."
+    exit 1
+fi
+scp -r *.html utils.jar doc/ deadsea@ostermiller.org:www/utils
