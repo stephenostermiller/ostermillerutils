39a5d28c2cb92d80c0e7b03e3985ba091c6d94b1
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2002-09-10 08:15:04 +0000

    Put utils.jar in a variable, added Base64 command line program.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 directory=/usr/local/bin
+utils=utils.jar
 RandPass=RandPass
 LineEnds=LineEnds
 MD5=MD5Sum
 Tabs=Tabs
+Base64=Base64
 
 if [ ! -z $1 ]
     then 
@@ -22,11 +24,11 @@ fi
 
 workingdir=`pwd`
 
-if [ ! -e $workingdir/utils.jar ]
+if [ ! -e $workingdir/$utils ]
 then
-    echo "Could not find 'utils.jar'."
+    echo "Could not find '$utils'."
     echo "Make sure you execute this script from"
-    echo "the directory that contains 'utils.jar'."
+    echo "the directory that contains '$utils'."
     exit 1
 fi
 
@@ -41,7 +43,7 @@ fi
 if [ ! -e $directory/$RandPass ] || [ ! -z $1 ]
 then
     echo "#!/bin/bash" > $directory/$RandPass
-    echo "java -classpath $workingdir/utils.jar com.Ostermiller.util.RandPass \$@" >> $directory/$RandPass
+    echo "java -classpath $workingdir/$utils com.Ostermiller.util.RandPass \$@" >> $directory/$RandPass
     chmod 755 $directory/$RandPass
     echo "$RandPass installed."
 else
@@ -51,7 +53,7 @@ fi
 if [ ! -e $directory/$LineEnds ] || [ ! -z $1 ]
 then
     echo "#!/bin/bash" > $directory/$LineEnds
-    echo "java -classpath $workingdir/utils.jar com.Ostermiller.util.LineEnds \$@" >> $directory/$LineEnds
+    echo "java -classpath $workingdir/$utils com.Ostermiller.util.LineEnds \$@" >> $directory/$LineEnds
     chmod 755 $directory/$LineEnds
     echo "$LineEnds installed."
 else
@@ -61,7 +63,7 @@ fi
 if [ ! -e $directory/$MD5 ] || [ ! -z $1 ]
 then
     echo "#!/bin/bash" > $directory/$MD5
-    echo "java -classpath $workingdir/utils.jar com.Ostermiller.util.MD5 \$@" >> $directory/$MD5
+    echo "java -classpath $workingdir/$utils com.Ostermiller.util.MD5 \$@" >> $directory/$MD5
     chmod 755 $directory/$MD5
     echo "$MD5 installed."
 else
@@ -71,11 +73,21 @@ fi
 if [ ! -e $directory/$Tabs ] || [ ! -z $1 ]
 then
     echo "#!/bin/bash" > $directory/$Tabs
-    echo "java -classpath $workingdir/utils.jar com.Ostermiller.util.Tabs \$@" >> $directory/$Tabs
+    echo "java -classpath $workingdir/$utils com.Ostermiller.util.Tabs \$@" >> $directory/$Tabs
     chmod 755 $directory/$Tabs
     echo "$Tabs installed."
 else
     echo "$directory/$Tabs already exists.  Use -f to overwrite."
+fi
+
+if [ ! -e $directory/$Base64 ] || [ ! -z $1 ]
+then
+    echo "#!/bin/bash" > $directory/$Base64
+    echo "java -classpath $workingdir/$utils com.Ostermiller.util.Base64 \$@" >> $directory/$Base64
+    chmod 755 $directory/$Base64
+    echo "$Base64 installed."
+else
+    echo "$directory/$Base64 already exists.  Use -f to overwrite."
 fi
 
 
