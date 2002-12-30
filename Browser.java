590514f3dd716be2dee6b000a40ffccb235ab357
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2002-12-30 19:19:24 +0000

    Chanded copyright.

--- Browser.java
+++ Browser.java
@@ -1,6 +1,6 @@
 /*
  * Control a web browser from your java application.
- * Copyright (C) 2001 Stephen Ostermiller
+ * Copyright (C) 2001-2002 Stephen Ostermiller
  * http://ostermiller.org/contact.pl?regarding=Java+Utilities
  *
  * This program is free software; you can redistribute it and/or modify
