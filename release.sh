d8bfae27a5cf1d1f7c9f6b3707c68100d66004ac
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2003-09-01 02:03:34 +0000

    version 1.02.16

--- release.sh
+++ release.sh
@@ -2,13 +2,16 @@
 
 size=`ls -lah utils.jar`
 size=${size:38:4}
-if [ -zi "`grep $size index.html`" ]
+if [ -z "`grep -i $size download.html`" ]
 then
-    echo "utils.jar size is $size but index.html does not show that."
+    echo "utils.jar size is $size but download.html does not show that."
     exit 1
 fi
 
-FILES=$@
+latestversion=`grep -oE 'ostermillerutils_[0-9]_[0-9]{2}_[0-9]{2}\.jar' download.bte | sort | tail -1`
+cp utils.jar "$latestversion"
+
+FILES="$@ $latestversion"
 FILES=${FILES/package.html/} 
 if [ "$FILES" ]
 then
@@ -17,3 +20,5 @@ then
 	scp -r $FILES deadsea@ostermiller.org:www/utils
     chmod +x install.sh
 fi
+
+rm "$latestversion"
