62070a2493331d88676fa487a3e7ba2e31149853
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2007-02-13 19:29:24 +0000

    New exception for CSV

---
+++ BadLineEndingException.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2007 Stephen Ostermiller
+ * http://ostermiller.org/contact.pl?regarding=Java+Utilities
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See COPYING.TXT for details.
+ */
+
+package com.Ostermiller.util;
+
+/**
+ * An illegal line ending was specified.
+ *
+ * @author Stephen Ostermiller http://ostermiller.org/contact.pl?regarding=Java+Utilities
+ * @since ostermillerutils 1.06.01
+ */
+public class BadLineEndingException extends IllegalArgumentException {
+
+	/**
+	 * Constructs an exception with null as its error detail message.
+	 *
+	 * @since ostermillerutils 1.06.01
+	 */
+	public BadLineEndingException(){
+		super();
+	}
+
+	/**
+	 * Constructs an exception with the specified detail message.
+	 * The error message string s can later be retrieved by the
+	 * Throwable.getMessage()  method of class java.lang.Throwable.
+	 *
+	 * @param s the detail message.
+	 *
+	 * @since ostermillerutils 1.06.01
+	 */
+	public BadLineEndingException(String s){
+		super(s);
+	}
+}
