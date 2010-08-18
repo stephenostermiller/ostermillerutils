62c895dc1dcc0b2b0e054334438dc583d029a9c9
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2010-08-18 13:14:10 -0400

    Site changes

--- src/site/script/create_syntax_highlighted_source_code.pl
+++ src/site/script/create_syntax_highlighted_source_code.pl
@@ -7,66 +7,121 @@ if ($dir !~ /\/src\/site\/script\/?$/){
   die "Must be run from src/site/script/";
 }
 
+`mkdir -p ../xdoc/javadoc`;
+`mkdir -p ../apt/src`;
+
 my $sourceList = "";
+my $menuList = "";
+my $javadocList = "";
 
 my @files = split(/\n/, `find ../../main/ -type f`);
 for my $file (sort @files){
-  my ($path, $base, $ext) = $file =~ /^\.\.\/\.\.\/(.*\/)([^\/]+)(\.[^\/\.]+)/;
+  my ($path, $barename, $ext) = $file =~ /^\.\.\/\.\.\/(.*\/)([^\/]+)(\.[^\/\.]+)/;
   my $classpath = $path;
   $classpath =~ s/^[^\/]+\/[^\/]+\///g;
-  my $name = "$base$ext";
-  $file = "src/$path$name";
-  my $htmlname="$name.html";
-  my $aptvmname="$name.apt.vm";
+  my $basename = "$barename$ext";
+  my $mavenrootname = "src/$path$basename";
+  my $srchtmlhref="$basename.html";
+  my $srcAptVmFile="../apt/src/$basename.apt.vm";
   my $brush = "";
-  my $javadocLink = "";
-  my $docLink = "";
-  if ($name =~ /\.java$/){
+  my $javadocHref = "";
+  my $javadocAptLink = "";
+  my $javadocFile = "";
+  my $docAptLink = "";
+  my $docXdocLink = "";
+  if ($basename =~ /\.java$/){
     $brush = "|brush=java";
-    $javadocLink = "  * {{{./doc/$classpath$base.html}$base Javadoc}}";
+    my $public = `grep -c -E 'public .*(class|interface)' $file`;
+    if ($public > 0){
+      $javadocHref = "../doc/$classpath$barename.html";
+      $javadocAptLink = "  * {{{../javadoc/$barename.html}$barename Javadoc}}";
+      $javadocList .= "        <item name=\"$barename\" href=\"/javadoc/$barename.html\" />\n";
+      $javadocFile = "../xdoc/javadoc/$barename.xml";
+    }
   }
-  if (-f "../apt/$base.apt.vm"){
-    $docLink = "  * {{{./$base.html}$base Documentation and Examples}}"
+  my $srcXdocLink = "<li><a href=\"../src/$srchtmlhref\">$basename Source Code</a></li>";
+  if (-f "../apt/$barename.apt.vm"){
+    $docAptLink = "  * {{{../$barename.html}$barename Documentation and Examples}}";
+    $docXdocLink = "<li><a href=\"../$barename.html\">$barename Documentation and Examples</a></li>";
   }
   
-  open (FILE, ">../apt/$aptvmname") or die $?;
+  $sourceList .= "  * {{{./src/$srchtmlhref}$basename Source Code}}\n\n";
+  $menuList .= "        <item name=\"$basename\" href=\"/src/$srchtmlhref\" />\n";
+  
+  &createSrcFile($srcAptVmFile, $basename, $docAptLink, $javadocAptLink, $brush, $mavenrootname);
+  if ($javadocHref){
+    &createJavadocFile($javadocFile, $barename, $javadocHref, $docXdocLink, $srcXdocLink);
+  }
+}
+
+&replaceInFile("../apt/source.apt.vm", "GENERATED SOURCES LIST", $sourceList);
+&replaceInFile("../site.xml", "GENERATED SOURCES MENU", $menuList);
+&replaceInFile("../site.xml", "GENERATED JAVADOC MENU", $javadocList);
+
+sub createSrcFile(){
+  my ($srcAptVmFile, $basename, $docAptLink, $javadocAptLink, $brush, $mavenrootname) = @_;
+  open (FILE, ">$srcAptVmFile") or die $?;
   print FILE "  ----
-  $name Source Code
+  $basename Source Code
   ----
   Stephen Ostermiller;
-  meta-description=View the source code for $name from the OstermillerUtils Java utilities.;
+  meta-description=View the source code for $basename from the OstermillerUtils Java utilities.;
   ----
 
-$docLink
+$basename Source Code
 
-$javadocLink
+$docAptLink
 
-$name Source Code
+$javadocAptLink
 
-\%{code-snippet$brush|file=$file}
+\%{code-snippet$brush|file=$mavenrootname}
 ";
 
   close (FILE);
   
-  $sourceList .= "  * {{{./$htmlname}$name Source Code}}\n\n";
+}
+
+sub createJavadocFile(){
+  my ($javadocFile, $barename, $javadocHref, $docXdocLink, $srcXdocLink) = @_;
+  open (FILE, ">$javadocFile") or die $?;
+  print FILE "
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<document xmlns=\"http://maven.apache.org/XDOC/2.0\"
+  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+  xsi:schemaLocation=\"http://maven.apache.org/XDOC/2.0 http://maven.apache.org/xsd/xdoc-2.0.xsd\">
+  <properties>
+    <title>$barename OstermillerUtils JavaDoc</title>
+    <author>Stephen Ostermiller</author>
+  </properties>
+  <head>
+    <meta name=\"description\" content=\"View JavaDoc for $barename from the com.Ostermiller.util package.\" />
+  </head>
+  <body>
+    <section name=\"$barename OstermillerUtils JavaDoc\">
+      <ul>
+        $docXdocLink
+        $srcXdocLink
+      </ul>
+      <iframe src=\"$javadocHref\" width=\"100%\" height=\"800\"></iframe>
+    </section>
+  </body>
+</document>
+";
+  close (FILE);
   
 }
 
+sub replaceInFile(){
+  my ($filename, $section, $generated) = @_;
   
-open (FILE, ">../apt/source.apt.vm") or die $?;
-print FILE "  ----
-  Source Code
-  ----
-  Stephen Ostermiller;
-  meta-description=View the source code from the OstermillerUtils Java utilities.;
-  ----
-
-Source Code -- Java Utilities - OstermillerUtils
-
-  A list of all source files for the Ostermiller Java Utilites appears below. Each has been has syntax highlighting for easy reading. All the source code is also included in the downloadable .jar file. 
-
-$sourceList
-";
-
-close (FILE);
-
+  open(FILE, $filename) or die $?;
+  my $contents = "";
+  while (my $line = <FILE>){
+    $contents .= $line;
+  }
+  close(FILE);
+  $contents =~ s/(BEGIN $section[^\r\n]*)(?:.|[\r\n])*?([^\r\n]*END $section)/\1\n$generated\2/g;
+  open(FILE, ">$filename") or die $?;
+  print FILE $contents;
+  close(FILE);  
+}
