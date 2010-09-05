4fd3784b451b666f544a50204b49b4c7dc40b7da
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2010-09-05 15:23:10 -0400

    Updated copyright

--- src/main/java/com/Ostermiller/util/CSVPrinter.java
+++ src/main/java/com/Ostermiller/util/CSVPrinter.java
@@ -1,7 +1,7 @@
 /*
- * Write files in comma separated value format.
  * Copyright (C) 2001-2010 Stephen Ostermiller
  * http://ostermiller.org/contact.pl?regarding=Java+Utilities
+ * 
  * Copyright (C) 2003 Pierre Dittgen <pierre dot dittgen at pass-tech dot fr>
  *
  * This program is free software; you can redistribute it and/or modify
