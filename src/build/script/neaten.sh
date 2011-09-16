c34306aaabf1883bc2761361f0f787dacc6eb046
Stephen Ostermiller <stephenostermiller@users.noreply.github.com>
2011-09-16 09:47:51 -0400

    Updated copyright

--- src/build/script/neaten.sh
+++ src/build/script/neaten.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2010 Stephen Ostermiller
+# Copyright (C) 2010-2011 Stephen Ostermiller
 # http://ostermiller.org/contact.pl?regarding=Java+Utilities
 #
 # This program is free software; you can redistribute it and/or modify
